88d35ea24f3923a8868138b6b9db0831b0cebcf3
toni <toni@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-10-15 15:21:10 +0000

    NEXUS-2833 fixed ReIndexTest
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/luceneindexer@5296 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus-luceneindexer-contrib/src/main/java/org/sonatype/nexus/index/context/DocumentFilter.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2007-2008 Sonatype, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package org.sonatype.nexus.index.context;
+
+import org.apache.lucene.document.Document;
+
+// TODO TONI - copied from indexer where doc is a lucene document. 
+public interface DocumentFilter
+{
+    boolean accept( Document doc );
+}
